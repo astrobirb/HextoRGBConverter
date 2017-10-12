d2bdaa5a2a75d757d70208d24580cec44a6d9f9a
astrobirb <31900770+astrobirb@users.noreply.github.com>
2017-10-12 13:24:28 -0600

    Added comments

--- MainActivity.cs
+++ MainActivity.cs
@@ -9,6 +9,7 @@ namespace HextoRGBConverter
     public class MainActivity : Activity
     {
 
+        // Assigned IDs for app's components
         EditText hexValueEditText;
         TextView redTextView, greenTextView, blueTextView;
         Button calculateButton;
@@ -21,6 +22,7 @@ namespace HextoRGBConverter
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
+            // Link to IDs
             hexValueEditText = FindViewById<EditText>(Resource.Id.hexValueEditText);
             redTextView = FindViewById<TextView>(Resource.Id.redTextView);
             greenTextView = FindViewById<TextView>(Resource.Id.greenTextView);
@@ -28,24 +30,30 @@ namespace HextoRGBConverter
             calculateButton = FindViewById<Button>(Resource.Id.calculateButton);
             colorView = FindViewById<View>(Resource.Id.colorView);
 
+            // Convert button
             calculateButton.Click+= CalculateButton_Click;
         }
 
+        // Activates when the button is pressed
         private void CalculateButton_Click(object sender, System.EventArgs e)
         {
+            // Variables - refer to iOS version
             string hexValue = hexValueEditText.Text;
             string redHexValue = hexValue.Substring(0, 2);
             string greenHexValue = hexValue.Substring(2, 2);
             string blueHexValue = hexValue.Substring(4, 2);
 
+            // Value converters - similar to iOS version
             int redValue = int.Parse(redHexValue, System.Globalization.NumberStyles.HexNumber);
             int greenValue = int.Parse(greenHexValue, System.Globalization.NumberStyles.HexNumber);
             int blueValue = int.Parse(blueHexValue, System.Globalization.NumberStyles.HexNumber);
 
+            // Displays converted values - similar to iOS version
             redTextView.Text = redValue.ToString();
             greenTextView.Text = greenValue.ToString();
             blueTextView.Text = blueValue.ToString();
 
+            // Invisible colour changing area - similar to iOS version
             colorView.SetBackgroundColor(new Android.Graphics.Color(redValue, greenValue, blueValue));
         }
     }
